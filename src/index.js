b59b68b56505edbdad4b0fb6b5c2a6cbcaa327b1
isabelly Oliveira <165833812+BellyOliveira@users.noreply.github.com>
2025-02-25 21:35:36 +0000

    Add - Nome

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import roteadorLogin from "./routes/login.js";
+import roteadorUsuario from "./routes/usuarios.js";
+
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();                        // Instancia o Express
+const port = 3000;                            // Define a porta
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  
+app.use(cors());  
+app.use(roteadorUsuario);
+app.use(roteadorLogin);
+
+// Rota raiz
+app.get("/", ( req, res) => {
+  console.log("Rota GET/ solicitada");
+  res.json({
+    nome: "Luis Henrique",                   // Substitua pelo seu nome
+  });
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Serviço escutando na porta: ${port}`);
+});
